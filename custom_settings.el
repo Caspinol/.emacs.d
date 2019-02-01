05cf341dcf36e793a2771e049085d34576e2d526
Kris <kristhesavage@gmail.com>
2019-02-02 00:30:53 +0100

    Added stuff for Vue development

--- custom_settings.el
+++ custom_settings.el
@@ -7,11 +7,11 @@
    ["#3f3f3f" "#ea3838" "#7fb07f" "#fe8b04" "#62b6ea" "#e353b9" "#1fb3b3" "#d5d2be"])
  '(custom-safe-themes
    (quote
-    ("a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" "ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" "04dd0236a367865e591927a3810f178e8d33c372ad5bfef48b5ce90d4b476481" "7356632cebc6a11a87bc5fcffaa49bae528026a78637acd03cae57c091afd9b9" "47ec21abaa6642fefec1b7ace282221574c2dd7ef7715c099af5629926eb4fd7" default)))
+	("a0feb1322de9e26a4d209d1cfa236deaf64662bb604fa513cca6a057ddf0ef64" "ab04c00a7e48ad784b52f34aa6bfa1e80d0c3fcacc50e1189af3651013eb0d58" "04dd0236a367865e591927a3810f178e8d33c372ad5bfef48b5ce90d4b476481" "7356632cebc6a11a87bc5fcffaa49bae528026a78637acd03cae57c091afd9b9" "47ec21abaa6642fefec1b7ace282221574c2dd7ef7715c099af5629926eb4fd7" default)))
  '(diary-entry-marker (quote font-lock-variable-name-face))
  '(emms-mode-line-icon-image-cache
    (quote
-    (image :type xpm :ascent center :data "/* XPM */
+	(image :type xpm :ascent center :data "/* XPM */
 static char *note[] = {
 /* width height num_colors chars_per_pixel */
 \"    10   11        2            1\",
@@ -35,7 +35,7 @@ static char *note[] = {
  '(gnus-logo-colors (quote ("#2fdbde" "#c0c0c0")) t)
  '(gnus-mode-line-image-cache
    (quote
-    (image :type xpm :ascent center :data "/* XPM */
+	(image :type xpm :ascent center :data "/* XPM */
 static char *gnus-pointer[] = {
 /* width height num_colors chars_per_pixel */
 \"    18    13        2            1\",
@@ -58,28 +58,28 @@ static char *gnus-pointer[] = {
 \"###########.######\" };")) t)
  '(package-selected-packages
    (quote
-    (org-bullets rainbow-mode fzf alect-themes markdown-preview-eww flycheck-mmark ox-hugo toml-mode cargo company-lsp rust-mode flycheck-irony company-irony irony prettier-js git-gutter magit exec-path-from-shell babel font-lock dired json-mode web-mode flycheck flycheck-rust company js2-mode auto-compile powerline autopair color-theme use-package)))
+	(vue-mode vue-html-mode org-bullets rainbow-mode fzf alect-themes markdown-preview-eww flycheck-mmark ox-hugo toml-mode cargo company-lsp rust-mode flycheck-irony company-irony irony prettier-js git-gutter magit exec-path-from-shell babel font-lock dired json-mode web-mode flycheck flycheck-rust company js2-mode auto-compile powerline autopair color-theme use-package)))
  '(vc-annotate-background "#222222")
  '(vc-annotate-color-map
    (quote
-    ((20 . "#db4334")
-     (40 . "#ea3838")
-     (60 . "#abab3a")
-     (80 . "#e5c900")
-     (100 . "#fe8b04")
-     (120 . "#e8e815")
-     (140 . "#3cb370")
-     (160 . "#099709")
-     (180 . "#7fb07f")
-     (200 . "#32cd32")
-     (220 . "#8ce096")
-     (240 . "#528d8d")
-     (260 . "#1fb3b3")
-     (280 . "#0c8782")
-     (300 . "#30a5f5")
-     (320 . "#62b6ea")
-     (340 . "#94bff3")
-     (360 . "#e353b9"))))
+	((20 . "#db4334")
+	 (40 . "#ea3838")
+	 (60 . "#abab3a")
+	 (80 . "#e5c900")
+	 (100 . "#fe8b04")
+	 (120 . "#e8e815")
+	 (140 . "#3cb370")
+	 (160 . "#099709")
+	 (180 . "#7fb07f")
+	 (200 . "#32cd32")
+	 (220 . "#8ce096")
+	 (240 . "#528d8d")
+	 (260 . "#1fb3b3")
+	 (280 . "#0c8782")
+	 (300 . "#30a5f5")
+	 (320 . "#62b6ea")
+	 (340 . "#94bff3")
+	 (360 . "#e353b9"))))
  '(vc-annotate-very-old-color "#e353b9"))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
